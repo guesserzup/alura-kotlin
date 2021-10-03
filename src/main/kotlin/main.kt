6f42c15914d6aec256cf062463b641f7797430d0
Gabriel Guesser <gdguesser@gmail.com>
2021-10-03 10:20:36 -0300

    condicional

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,22 @@
 fun main() {
 
     println("Curso Kotlin Alura")
+
     val titular = "Gabriel Guesser"
+    val accountNumber: Int = 82126946
+
+    var saldo: Double = 0.0
+    saldo = 100.0
 
     println("Olá, $titular")
+    println("Número da conta $accountNumber")
+    println("Saldo da conta $saldo")
+
+    when {
+        saldo > 0.0 -> println("conta é positiva!")
+
+        saldo == 0.0 -> println("conta neutra!")
+
+        else -> println("conta está negativa!")
+    }
 }
