67f11c6654bcfa13e0ac2a7f707b5cd8515abfb8
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-10-04 09:49:13 -0300

    class and immutability

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,6 +16,22 @@ fun main() {
     for (i in 10 downTo 0 step 5) {
         println(i)
     }
+
+    val conta1 = Conta()
+    conta1.titular = "Gabriel"
+
+    val conta2 = Conta()
+    conta2.titular = "Alex"
+
+    println(conta1.titular)
+    println(conta2.titular)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.00
+
 }
 
 fun testaCondicoes(saldo: Double) {
